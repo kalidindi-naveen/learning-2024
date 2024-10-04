ef2ae9bab668594f3185070956eecf9fafa17768
kalidindi-naveen <151353708+kalidindi-naveen@users.noreply.github.com>
2024-10-04 09:55:30 +0530

    Added jenkins-install.sh

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -13,6 +13,10 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-apt-get update -y
+apt-get update -y 
 
-apt-get install jenkins -y
+apt-get install jenkins -y 
+
+systemctl enable jenkins
+
+systemctl restart jenkins

e086414638535d987061cf9f0f8555d3dcaa0079
kalidindi-naveen <151353708+kalidindi-naveen@users.noreply.github.com>
2024-10-04 10:20:31 +0530

    Added trivy-install.sh

---
+++ trivy-install.sh
@@ -0,0 +1,5 @@
+apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+apt-get update -y
+apt-get install trivy -y
